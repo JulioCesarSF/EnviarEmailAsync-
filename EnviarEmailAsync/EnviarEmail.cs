80c1b4f106e243c158d7a10d8aecdbe7f42c627a
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2017-10-26 21:52:47 -0200

    Add project files.

---
+++ EnviarEmailAsync/EnviarEmail.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnviarEmailAsync
+{
+    class Email
+    {
+        public string De { get; set; }
+        public string Para { get; set; }
+        public string Assunto { get; set; }
+        public string Mensagem { get; set; }
+        public ConfiguracaoEmail Configuracao { get; set; }
+    }
+
+    class ConfiguracaoEmail
+    {
+        public string Usuario { get; set; }
+        public string Senha { get; set; }
+        public string Servidor { get; set; }
+        public int Porta { get; set; }
+    }
+    class EnviarEmail
+    {
+        private SmtpClient _smtp { get; set; }
+        private Email _email { get; set; }
+        private MailMessage _mensagem { get; set; }
+
+        public EnviarEmail(Email email)
+        {
+            _email = email;
+            if (email == null)
+                throw new Exception("Email é null, passar um objeto");
+            else if (email != null && email.Configuracao == null)
+                throw new Exception("Configuração de email é null, passar um objeto");
+            else
+                _mensagem = Build();
+        }
+
+        private MailMessage Build()
+        {
+            MailMessage mensagem = new MailMessage();
+
+            //encode do email para não ter problemas com caracters
+            mensagem.HeadersEncoding = Encoding.UTF8;
+            mensagem.BodyEncoding = Encoding.UTF8;
+            mensagem.SubjectEncoding = Encoding.UTF8;
+
+            //de quem é o email
+            mensagem.From = new MailAddress(_email.De);
+
+            //para quem é o email
+            //pode passar uma lista dividida por ';'   ex: email@email.com;email2@email.com;email3@email.com
+            MailAddressCollection para = new MailAddressCollection();
+            foreach (var email in _email.Para.Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                para.Add(new MailAddress(email));
+            }
+            mensagem.To.Add(para.ToString());
+
+            //assunto
+            mensagem.Subject = _email.Assunto;
+            //mensagem é um html
+            mensagem.IsBodyHtml = true;
+            //corpo do email
+            mensagem.Body = _email.Mensagem;
+            //prioridade do email
+            mensagem.Priority = MailPriority.Normal;            
+
+            //configurar o smtp
+            _smtp = new SmtpClient()
+            {
+                Host = _email.Configuracao.Servidor,
+                Port = _email.Configuracao.Porta,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential(_email.Configuracao.Usuario, _email.Configuracao.Senha)
+            };
+
+            return mensagem;
+        }
+
+        public async Task<bool> EnviarAsync()
+        {
+            try
+            {                
+                using (_smtp)
+                {
+                    //adicionar a callback de envio do email
+                    _smtp.SendCompleted += new SendCompletedEventHandler(EnviarCompleto);
+                    using (_mensagem)
+                        await _smtp.SendMailAsync(_mensagem);
+                }               
+
+                return true;
+            }
+            catch (ArgumentNullException aE)
+            {
+                Console.WriteLine("Falta de parametros na mensagem: {0}", aE.Message);
+            }
+            catch (SmtpException sE)
+            {
+                Console.WriteLine("SmtpException: {0}", sE.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return false;
+
+        }
+
+        public async Task EnviarSemRetornoAsync()
+        {
+            try
+            {
+                using (_smtp)
+                {
+                    //adicionar a callback de envio do email
+                    _smtp.SendCompleted += new SendCompletedEventHandler(EnviarCompleto);
+                    using (_mensagem)
+                        await _smtp.SendMailAsync(_mensagem);
+                }
+                
+            }
+            catch (ArgumentNullException aE)
+            {
+                Console.WriteLine("Falta de parametros na mensagem: {0}", aE.Message);
+            }
+            catch (SmtpException sE)
+            {
+                Console.WriteLine("SmtpException: {0}", sE.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void CancelarEnvio()
+        {
+            if (_smtp != null)
+            {
+                _smtp.SendAsyncCancel();
+            }
+        }
+
+        private void EnviarCompleto(object sender, AsyncCompletedEventArgs e)
+        {
+            _mensagem = e.UserState as MailMessage;
+
+            if (!e.Cancelled && e.Error == null)
+            {
+                //adicionar log de envio no banco                
+                Console.WriteLine("Email enviado com sucesso!");
+            }
+            else if (e.Error != null)
+            {
+                //adicionar log do erro no banco
+                Console.WriteLine("Ocorreu um erro ao enviar o email: {0}", e.Error.ToString());
+            }
+            else if (e.Cancelled)
+            {
+                //adicionar log do cancelamento no banco
+                Console.WriteLine("Envio de email cancelado!");
+            }
+
+            //limpar
+            if (_mensagem != null)
+                _mensagem.Dispose();
+        }
+    }
+}
---
+++ EnviarEmailAsync/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+  Servidor: smtps.uol.com.br
+    Porta: 587
+ */
+
+namespace EnviarEmailAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //configuraçoes do smtp
+            ConfiguracaoEmail cfg = new ConfiguracaoEmail()
+            {
+                Usuario = "julio.com@uol.com.br",
+                Senha = "2diol",
+                Porta = 587,
+                Servidor = "smtps.uol.com.br"
+            };
+
+            //configurar o email a ser enviado
+            Email email = new Email()
+            {
+                Configuracao = cfg,
+                De = cfg.Usuario,
+                Para = "jcschincariolfh@gmail.com",
+                Assunto = "Teste de envio",
+                Mensagem = "<html>" +
+                            "<header><title>Titulo</title></header>" +
+                            "<body><h1>Teste H1</h1></body>" +
+                            "</html>"
+            };
+
+            try
+            {
+                var enviar = new EnviarEmail(email);
+                Task<bool> retornoEnviar = enviar.EnviarAsync();
+
+                //pegar o retorno fora do async se precisar
+                bool enviado = retornoEnviar.Result;
+                if(enviado)
+                    Console.WriteLine("Enviado.");
+                else
+                    Console.WriteLine("Não enviado.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro ao inicializar EnviarEmail: {0}", e.Message);
+                throw;
+            }
+
+            Console.WriteLine("Executando outra ação enquanto envia o email.");
+            for (int i = 1; i <= 10000; i++)
+            {
+                //if(i == 500) //testar o cancelamento
+                //    enviar.CancelarEnvio();
+                Console.Write("\r{0} %", i);
+            }
+
+            Console.Read();
+
+        }
+    }
+}
